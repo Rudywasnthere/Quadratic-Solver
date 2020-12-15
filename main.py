ff2489aefd3c73992d9dbb110ec49a6788b9f0f2
Rudywasnthere <rudybcchs@gmail.com>
2020-12-15 18:32:17 +0000

    yes

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if determinant >0:
   end = "You have two solutions"
 
 def solver(a,b,c,determinant):
+  count = 0
   if determinant >=0:
     try:
       determinant = int(determinant**0.5)
@@ -26,16 +27,18 @@ def solver(a,b,c,determinant):
       solution_1 = f"{-b/(2*a)} + {(determinant)}**0.5/{(2*a)}"
       solution_2 = f"{-b/(2*a)} - {(determinant)}**0.5/{(2*a)}"
   if determinant <0:
-    x = determinant**0.5
+    x = str(determinant**0.5)
     x = x.split("+")
     x = x[1]
     x = x.replace('j)', 'i')
     solution_1 = f"{-b/(2*a)} + {x}"
-    solution_2 = f"{-b(2*a)} - {x}"
-  return solution_1, solution_2
+    solution_2 = f"{-b/(2*a)} - {x}"
+    count += 1
+  return solution_1, solution_2, count
 
-one , two = solver(a,b,c, determinant)
-one = f"({one},0)"
-if two != "":
-  two = f"({two},0)"
+one , two , count = solver(a,b,c, determinant)
+if count == 0:
+  one = f"({one},0)"
+  if two != "" :
+    two = f"({two},0)"
 print(f"\n{end}\nYour solutions are:\n{one}\n{two}")
